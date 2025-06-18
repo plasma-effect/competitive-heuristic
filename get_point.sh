d3b40b1145c249b95b5073a3213075d2112e00f4
plasma-effect <mk_vertex3625_plasma@kcn.jp>
2025-06-18 17:21:42 +0900

    remove custom visualizer

--- get_point.sh
+++ get_point.sh
@@ -1,44 +1,20 @@
 #!/bin/bash
 set -uo pipefail
-TARGET_LESSER=0
-make main_measure.o check.o
+cmake --build build/release --target main
+cmake --build build/develop --target score_manager
 if [ $? -ne 0 ]; then
   exit 1
 fi
-mkdir -p tmp
+mkdir -p samples_out
 SUM=0
-ls samples/*.txt | parallel -k --bar --joblog parallel.log './main_measure.o < {} > samples_out/$(basename {}) 2>/dev/null'
+ls samples/*.txt | parallel -k --bar --joblog parallel.log './build/release/main < {} > samples_out/$(basename {}) 2>/dev/null'
 if [ $? -ne 0 ]; then
   awk 'NR > 1 && $7 != 0 { print $9 }' parallel.log
   exit 1
 fi
 for file in $(ls samples/*.txt); do
-  NOW=$(cat ${file} samples_out/$(basename ${file}) | ./check.o)
+  NOW=$(./tools/target/release/vis ${file} samples_out/$(basename ${file}))
   SUM=$((SUM+NOW)) 
 done
 
-if [ -e best.txt ]; then
-  BEST=$(cat best.txt)
-elif [ ${TARGET_LESSER} -ge "1" ]; then
-  BEST=1000000000
-else
-  BEST=0
-fi
-
-if [ ${TARGET_LESSER} -ge "1" ]; then
-  if [ ${BEST} -gt ${SUM} ]; then
-    echo "updated the best score by $((BEST-SUM))"
-    echo "new best score is ${SUM}"
-    echo ${SUM} > best.txt
-  else
-    echo "${SUM} (best score = ${BEST}, diff = $((SUM-BEST)))"
-  fi
-else
-  if [ ${BEST} -lt ${SUM} ]; then
-    echo "updated the best score by $((SUM-BEST))"
-    echo "new best score is ${SUM}"
-    echo ${SUM} > best.txt
-  else
-    echo "${SUM} (best score = ${BEST}, diff = $((BEST-SUM)))"
-  fi
-fi
+./build/develop/score_manager score_setting.json $((SUM))
